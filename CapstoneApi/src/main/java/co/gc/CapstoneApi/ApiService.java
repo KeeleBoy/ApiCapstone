f7eac28fb510f044476f8fcc7ef80d6d781ff4af
David Breitenbeck <david.breitenbeck@yahoo.com>
2020-03-05 12:06:17 -0500

    mess

--- CapstoneApi/src/main/java/co/gc/CapstoneApi/ApiService.java
+++ CapstoneApi/src/main/java/co/gc/CapstoneApi/ApiService.java
@@ -1,13 +1,23 @@
 package co.gc.CapstoneApi;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import co.gc.CapstoneApi.entity.Movie;
+import co.gc.apidemostarter.model.Card;
+import co.gc.apidemostarter.model.DeckResponse;
+
 @Component
 public class ApiService {
+	
+	@Value("${api_key}")
+	String apiKey;
 
 	private RestTemplate rt;
 
@@ -20,7 +30,40 @@ public class ApiService {
 		};
 		rt = new RestTemplateBuilder().additionalInterceptors(interceptor).build();
 	}
+	
+	public List<Movie> searchMovie(String search) {
+		String url = "http://www.omdbapi.com/?apikey="+apiKey+"&?s="+search;
+		
+		Search search = rt.getForObject(url, Search.class);
+		
+		
+		
+//				 * + lon + "&FcstType=json";
+//				 * 
+//				 * Movie movie = rt.getForObject(url, Movie.class);
+//				 * 
+//				 * return movie;
+//		
+	}
+	
+	
+	public List<Card> drawCards(Integer count) {
 
+//		// generate your own key
+		String deckId = "s29ceirssuxj";
+
+		String url = "https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=" + count;
+
+		DeckResponse response = rt.getForObject(url, DeckResponse.class);
+
+		if(response.getRemaining()<1) {
+			
+		}
+		
+		return response.getCards();
+
+	}
+	
 	/*
 	 * public Movie findMovie() {
 	 * 
