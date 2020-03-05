735667debfd11150f7e7098b3f5a0ec451625912
kyle <kwarchuck@gmail.com>
2020-03-05 12:04:36 -0500

    fix

--- CapstoneApi/src/main/java/co/gc/CapstoneApi/CapstoneController.java
+++ CapstoneApi/src/main/java/co/gc/CapstoneApi/CapstoneController.java
@@ -3,6 +3,7 @@ package co.gc.CapstoneApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import co.gc.CapstoneApi.dao.MovieDao;
@@ -14,9 +15,26 @@ public class CapstoneController {
 	@Autowired
 	MovieDao movieDao;
 
+	@Autowired
+	private ApiService apiServ;
+
 	@RequestMapping("/")
 	public ModelAndView index() {
 		return new ModelAndView("index");
+	}
+
+	@RequestMapping("/movie")
+	public ModelAndView singleMovie(@RequestParam String title) {
+		return new ModelAndView("movie");
+
+	}
+
+	@RequestMapping("/search-results")
+	public ModelAndView searchResults(@RequestParam String search) {
+		ModelAndView mav = new ModelAndView("search-results");
+		// mav.addObject("search", apiServ);
+		return mav;
+
 	}
 
 }
---
+++ CapstoneApi/src/main/java/co/gc/CapstoneApi/entity/Search.java
@@ -0,0 +1,25 @@
+package co.gc.CapstoneApi.entity;
+
+import java.util.List;
+
+public class Search {
+
+	List<Movie> movies;
+
+	public List<Movie> getMovies() {
+		return movies;
+	}
+
+	public void setMovies(List<Movie> movies) {
+		this.movies = movies;
+	}
+
+	@Override
+	public String toString() {
+		return "Search [movies=" + movies + "]";
+	}
+
+	public Search() {
+	}
+
+}
