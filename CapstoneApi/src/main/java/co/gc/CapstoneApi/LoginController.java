6a8c63393712ff95fef5b226b29474a5a09ad441
kyle <kwarchuck@gmail.com>
2020-03-06 13:30:30 -0500

    test

---
+++ CapstoneApi/src/main/java/co/gc/CapstoneApi/LoginController.java
@@ -0,0 +1,62 @@
+package co.gc.CapstoneApi;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import co.gc.CapstoneApi.dao.MovieDao;
+import co.gc.CapstoneApi.entity.User;
+
+@Controller
+
+public class LoginController {
+
+	@Autowired
+	private MovieDao movieDao;
+
+	@Autowired
+	private HttpSession sesh;
+
+	@RequestMapping("/login")
+	public ModelAndView login() {
+		return new ModelAndView("login-form");
+
+	}
+
+	@PostMapping("/login")
+	public ModelAndView loginSubmit(@RequestParam("username") String username,
+			@RequestParam("password") String password, RedirectAttributes redir) {
+
+		User user = movieDao.findByUsernameIgnoreCase(username);
+
+		if (user == null || !password.equals(user.getPassword())) {
+			ModelAndView mav = new ModelAndView("login-form");
+			mav.addObject("message", "Wrong user name or password loser");
+			return mav;
+		}
+
+		sesh.setAttribute("user", user);
+
+		redir.addFlashAttribute("message", "Logged you in " + user.getName());
+
+		return new ModelAndView("redirect:/trainer/" + user.getId());
+
+	}
+
+	@RequestMapping("/logout")
+	public ModelAndView logout(RedirectAttributes redir) {
+
+		sesh.invalidate();
+
+		redir.addFlashAttribute("message", "logged you out :/");
+		return new ModelAndView("redirect:/login");
+
+	}
+
+}
--- CapstoneApi/src/main/java/co/gc/CapstoneApi/dao/MovieDao.java
+++ CapstoneApi/src/main/java/co/gc/CapstoneApi/dao/MovieDao.java
@@ -3,8 +3,11 @@ package co.gc.CapstoneApi.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.gc.CapstoneApi.entity.Movie;
+import co.gc.CapstoneApi.entity.User;
 
 public interface MovieDao extends JpaRepository<Movie, Long> {
 	
 	Movie findByTitleIgnoreCase(String name);
+
+	User findByUsernameIgnoreCase(String username);
 }
